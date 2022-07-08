3b75d835c19ad0d969ce4572dd8378e6ce8de940
jor-eldevera <jorell.devera@gmail.com>
2022-07-08 00:40:27 -0700

    Write game() which plays RPS then logs the winner

--- rps.js
+++ rps.js
@@ -58,3 +58,36 @@ function playRound(playerSelection, computerSelection) {
         console.error('Invalid playerSelection: ' + playerSelection);
     }
 }
+
+/**
+ * Plays five rounds of RPS then logs the winner at the end.
+ */
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // Run the game loop
+    for (let i = 0; i < 5; i++) {
+        let fullResult = playRound(prompt("Enter 'Rock', 'Paper', or 'Scissors'."), computerPlay());
+        console.log(fullResult);
+
+        let result = fullResult.charAt(4);
+        if (result === 'W') {
+            playerScore++;
+        } else if (result === 'L') {
+            computerScore++;
+        } else if (result === 'T') {
+            playerScore++;
+            computerScore++;
+        }
+    }
+
+    // Log the winner
+    if (playerScore > computerScore) {
+        console.log("Player wins! Score: " + playerScore + "-" + computerScore);
+    } else if (playerScore < computerScore) {
+        console.log("Player loses! Score: " + playerScore + "-" + computerScore);
+    } else {
+        console.log("Player ties computer! Score: " + playerScore + "-" + computerScore);
+    }
+}
