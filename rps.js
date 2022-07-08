cb88786d70c8750c44742fe07aa7237e8a1b9f80
jor-eldevera <jorell.devera@gmail.com>
2022-07-07 23:59:51 -0700

    Write playRound() which determines the winner between two selections

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+/**
+ * Randomly generates a hand sign.
+ * @returns {string} 'Rock', 'Paper', or 'Scissors'
+ */
 function computerPlay() {
     let num = Math.floor(Math.random() * 3);
     if (num === 0) {
@@ -6,5 +10,51 @@ function computerPlay() {
         return 'Paper';
     } else {
         return 'Scissors';
+    }
+}
+
+/**
+ * Given the player and computer's selections, determines who wins.
+ * @param {string} playerSelection The player's selection
+ * @param {string} computerSelection The computer's selection
+ * @returns A string stating whether the player won or lost and how they won or lost.
+ */
+function playRound(playerSelection, computerSelection) {
+    // Process playerSelection
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+
+    // Determine victor
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Paper') {
+            return 'You Lose! Paper beats Rock'
+        } else if (computerSelection === 'Scissors') {
+            return 'You Win! Rock beats Scissors'
+        } else if (computerSelection === 'Rock') {
+            return 'You Tie! Rock meets Rock'
+        } else {
+            console.error('Invalid computerSelection: ' + computerSelection);
+        }
+    } else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Paper') {
+            return 'You Tie! Paper meets Paper'
+        } else if (computerSelection === 'Scissors') {
+            return 'You Lose! Scissors beats Paper'
+        } else if (computerSelection === 'Rock') {
+            return 'You Win! Paper beats Rock'
+        } else {
+            console.error('Invalid computerSelection: ' + computerSelection);
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Paper') {
+            return 'You Win! Scissors beats Paper'
+        } else if (computerSelection === 'Scissors') {
+            return 'You Tie! Scissors meets Scissors'
+        } else if (computerSelection === 'Rock') {
+            return 'You Lose! Rock beats Scissors'
+        } else {
+            console.error('Invalid computerSelection: ' + computerSelection);
+        }
+    } else {
+        console.error('Invalid playerSelection: ' + playerSelection);
     }
 }
