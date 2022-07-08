22a5875efe7915ecd062633e359031e8623b1a1a
jor-eldevera <jorell.devera@gmail.com>
2022-07-07 23:29:43 -0700

    Write computerPlay() which returns 'Rock' 'Paper' or 'Scissors'

---
+++ rps.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0) {
+        return 'Rock';
+    } else if (num === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
